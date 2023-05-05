de72d53028ae42d98a6ec036d4c0fe33ba64479b
gal_sasi <31507494+galsasi1989@users.noreply.github.com>
2023-05-06 02:36:41 +0300

    Add modification to index.ts to support canary manifest

--- src/index.ts
+++ src/index.ts
@@ -5,44 +5,23 @@ export * as k8s from './imports/k8s';
 
 export interface AnalysisTemplate {
   readonly templateName: string;
-}
+};
 
 export interface ValueFrom {
   readonly podTemplateHashValue?: string;
   readonly fieldRef?: k8s.ObjectFieldSelector;
-}
+};
 
 export interface AnalysisArgs {
   readonly name: string;
   readonly value?: string;
   readonly valueFrom?: ValueFrom;
-}
+};
 
 export interface AnalysisSpec {
   readonly templates: AnalysisTemplate[];
   readonly args: AnalysisArgs[];
-}
-
-export type CanaryStaticStep = 
-  {
-    setWeight: number;
-  } | 
-  {
-    pause: { 
-      duration?: string 
-    }
-  } |
-  {
-    analysis: AnalysisSpec
-  } |
-  {
-    experiment: {
-      duration: string;
-      templates: { name: string; specRef: string }[];
-      analyses: { name: string; templateName: string }[];
-    }
-  }
-      
+};
 
 export type MatchTypes =
     {
@@ -55,18 +34,37 @@ export type MatchTypes =
       prefix: string;
     };
 
-  
-export type CanaryDynamicStep = 
+export type CanaryStep = 
   {
-    setCanaryScale: {
-       replicas: number
-    } | 
-    {
-       weight: number
-    } | {
-       matchTrafficWeight: boolean
-    }
+    setWeight: number;
   } | 
+  {
+    pause: { 
+      duration?: string;
+    }
+  } |
+  {
+    analysis: AnalysisSpec
+  } |
+  {
+    experiment: {
+      duration: string;
+      templates: { name: string; specRef: string }[];
+      analyses: { name: string; templateName: string }[];
+    }
+  } |
+  {
+    setCanaryScale: 
+      {
+        replicas: number
+      } |
+      {
+        weight: number
+      } |
+      {
+        matchTrafficWeight: boolean
+      }
+  } |
   {
     setHeaderRoute: {
       name: string;
@@ -81,17 +79,11 @@ export type CanaryDynamicStep =
       name: string;
       percentage: number;
       match: {
-        {
           method: MatchTypes;
-        } | 
-        {
           path: MatchTypes;
-        } |
-        {
           headers: {
             [key: string]: MatchTypes;
           }
-        }
       }[]
     }
   }
@@ -115,23 +107,30 @@ export interface BlueGreenStrategySpecs extends StrategySpecs {
 }
 
 export interface CanaryStrategySpecs extends StrategySpecs {
-   readonly canaryService: string;
-   readonly stableService: string;
-   readonly canaryMetadata: k8s.ObjectMeta;
-   readonly stableMetadata: k8s.ObjectMeta;
+   readonly canaryService?: string;
+   readonly stableService?: string;
+   readonly canaryMetadata?: k8s.ObjectMeta;
+   readonly stableMetadata?: k8s.ObjectMeta;
    readonly maxUnavailable?: number;
    readonly maxSurge?: string;
+   readonly minPodsPerReplicaSet?: number;
    readonly analysis?: AnalysisSpec;
-   readonly staticSteps?: CanaryStaticStep[]; //TODO: add type for steps
+   readonly steps?: CanaryStep[]; //TODO: add type for steps
    readonly TrafficRoutingParams?: {
-     dynamicSteps?: CanaryDynamicStep[];
-     nginxStableIngressNames?: string[];
+    nginx?: {
+      stableIngresses?: string[];
+      annotationPrefix?: string;
+      additionalIngressAnnotations?: {
+        'canary-by-header': string;
+        'canary-by-header-value:': string;
+      }
+     }
    }
 }
 
 export interface StrategySpecs {
   readonly blueGreen?: BlueGreenStrategySpecs;
-  readonly canary?: CanaryStrategySpecs;
+  canary?: CanaryStrategySpecs;
 }
 
 export interface ArgoSpecs {
@@ -172,9 +171,56 @@ export class ArgoRollout extends ApiObject {
    * @param props initialization props
    */
   public static manifest(props: ArgoRolloutProps): any {
+    let rolloutProps: any = props;
+    if(props.spec) {
+      if(props.spec.strategy.canary && props.spec.strategy.blueGreen) throw new Error('Strategy can be canary or bluegreen but not both');
+      if(props.spec.strategy.canary) {
+        if(props.spec.strategy.canary.TrafficRoutingParams) {
+          if (!props.spec.strategy.canary.canaryService) {
+            throw new Error('When Traffic routing is configured, the property canaryService is required');
+          }
+          if(!props.spec.strategy.canary.stableService) {
+            throw new Error('When Traffic routing is configured, the property stableService is required');
+          }
+        }
+        rolloutProps = {
+          canary: {
+            canaryService: props.spec.strategy.canary.canaryService,
+            stableService: props.spec.strategy.canary.stableService,
+            canaryMetadata: props.spec.strategy.canary.canaryMetadata ?? {
+              annotations: {
+                role: 'canary'
+              },
+              labels: {
+                role: 'canary'
+              }
+            },
+            stableMetadata: props.spec.strategy.canary.stableMetadata ?? {
+              annotations: {
+                role: 'stable'
+              },
+              labels: {
+                role: 'stable'
+              }
+            },
+            maxUnavailable: props.spec.strategy.canary.maxUnavailable ?? 1,
+            maxSurge: props.spec.strategy.canary.maxSurge ?? "20%",
+            scaleDownDelaySeconds: props.spec.strategy.canary.scaleDownDelaySeconds ?? 30,
+            minPodsPerReplicaSet: props.spec.strategy.canary.minPodsPerReplicaSet ?? (props.spec.strategy.canary.TrafficRoutingParams ? 2 : 1),
+            scaleDownDelayRevisionLimit: props.spec.strategy.canary.scaleDownDelayRevisionLimit,
+            analysis: props.spec.strategy.canary.analysis,
+            steps: props.spec.strategy.canary.steps,
+            antiAffinity: props.spec.strategy.canary.antiAffinity,
+            trafficRouting: props.spec.strategy.canary.TrafficRoutingParams,
+            abortScaleDownDelaySeconds: props.spec.strategy.canary.abortScaleDownDelaySeconds ?? 30
+          }
+        }
+      }
+    }
+
     return {
       ...ArgoRollout.GVK,
-      ...props,
+      ...rolloutProps,
     };
   }
 
